14898de0f85419cfbf445ee9bf7ee520a0417b7a
Ethan Ward <S182964@ucs.ac.uk>
2016-10-26 11:17:13 +0100

    Scripts re-uploaded
    Basic movement and teleporting scripts re-uploaded from the right
    account

---
+++ Assets - Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 5;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Movement
+		var move = new Vector3 (Input.GetAxis("Horizontal"),0);
+		transform.position += move*speed*Time.deltaTime;
+
+		}
+	}
