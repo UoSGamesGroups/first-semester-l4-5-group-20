8e2e6ce529b521dca247ad9660fba3afe5c06bb3
E-Ward <s182964@ucs.ac.uk>
2016-11-06 19:57:00 +0000

    Scripts for the asteroid like game in our game
    these are the completed scripts for the movement and the shooting

---
+++ Assets - Scripts/Minigame Scripts/Fire.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Set the bullet to destroy itself after 1 seconds
+		Destroy (gameObject, 1.0f);
+
+		// Push the bullet in the direction it is facing
+		GetComponent<Rigidbody2D>()
+			.AddForce(transform.up * 400);
+	}
+
+}
