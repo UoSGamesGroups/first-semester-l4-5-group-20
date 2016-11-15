add31e141a36e376c0172377f3fdc084e6bbf223
Charlie Crewe <C.Crewe@uos.ac.uk>
2016-11-15 21:29:50 +0000

    Ability Scripts
    Uploading the shield and overload scripts

---
+++ Assets - Scripts/Overload.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overload : MonoBehaviour 
+{
+	bool OverloadCD = true;
+	public GameObject PlasmaAOE;
+	
+	void Start () 
+	{
+		PlasmaAOE = GameObject.Find ("PlasmaAOE");
+	}
+	
+	void Update () 
+	{
+		if ((OverloadCD) && (Input.GetKeyDown (KeyCode.Q)))
+		{
+			Debug.Log("Q Was Pressed");
+			OverloadActive();
+		}
+		else if ((!OverloadCD))
+		{
+			PlasmaAOE.renderer.enabled = false;
+			StartCoroutine(OverloadRecharge());
+		}
+	}
+
+	IEnumerator OverloadRecharge ()
+	{
+		yield return new WaitForSeconds (45);
+		OverloadCD = true;
+	}
+
+	void OverloadActive ()
+	{
+		PlasmaAOE.renderer.enabled = true;
+		OverloadCD = false;
+	}
+}
---
+++ Assets - Scripts/Shield.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour 
+{
+	bool ShieldsUp = false;
+	bool ShieldCD = true;
+	public GameObject PlasmaShield;
+
+	void Start () 
+	{
+		PlasmaShield = GameObject.Find ("PlasmaShield");
+	}
+
+	void Update () 
+	{
+		if ((ShieldCD) && (Input.GetKeyDown (KeyCode.Space)) && (!ShieldsUp))
+		{
+			Debug.Log("Space Was Pressed");
+			StartCoroutine(ShieldActive());
+		}
+		else if ((!ShieldCD) && (!ShieldsUp))
+		{
+			StartCoroutine(ShieldRecharge());
+		}
+	}
+
+	IEnumerator ShieldActive ()
+	{
+		ShieldsUp = true;
+		PlasmaShield.renderer.enabled = true;
+		yield return new WaitForSeconds (5);
+		PlasmaShield.renderer.enabled = false;
+		ShieldCD = false;
+		ShieldsUp = false;
+	}
+
+	IEnumerator ShieldRecharge ()
+	{
+		yield return new WaitForSeconds (10);
+		ShieldCD = true;
+	}
+}
