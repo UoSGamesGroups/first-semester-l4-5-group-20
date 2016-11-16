b22694d745830cf61b4013cd8c7c39c58d56b47a
E-Ward <s182964@ucs.ac.uk>
2016-11-16 08:46:19 +0000

    Mouse aim
    Bullet shoots in the direction of the mouse

---
+++ Assets - Scripts/MouseFire.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFire : MonoBehaviour {
+
+
+	public GameObject bullet;
+	public float speed = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			Vector2 target = Camera.main.ScreenToWorldPoint( new Vector2(Input.mousePosition.x,  Input.mousePosition.y) );
+			Vector2 myPos = new Vector2(transform.position.x,transform.position.y + 1);
+			Vector2 direction = target - myPos;
+			direction.Normalize();
+			Quaternion rotation = Quaternion.Euler( 0, 0, Mathf.Atan2 ( direction.y, direction.x ) * Mathf.Rad2Deg );
+			GameObject projectile = (GameObject) Instantiate( bullet, myPos, rotation);
+			projectile.GetComponent<Rigidbody2D>().velocity = direction * speed;
+		}
+	
+	}
+}
