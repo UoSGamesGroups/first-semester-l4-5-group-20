655d923078b0a29df8da047ab5da4178798cb88a
E-Ward <s182964@ucs.ac.uk>
2016-11-22 15:53:29 +0000

    Movement and bullet deleting
    Scripts for movement and destroying the bullet when it goes off of the
    screen

---
+++ Assets - Scripts/Direction.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Direction : MonoBehaviour {
+
+	private float speed = 0.1f;
+	Vector2 vector = Vector2.up;
+	Vector2 moveVector;
+
+	void Start () {
+		InvokeRepeating("Movement", 0.3f, speed);
+
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			vector = Vector2.right;
+		} else if (Input.GetKey (KeyCode.W)) {
+			vector = Vector2.up;
+		} else if (Input.GetKey (KeyCode.S)) {
+			vector = -Vector2.up;
+		} else if (Input.GetKey (KeyCode.A)) {
+			vector = -Vector2.right;
+		}
+		moveVector = vector / 3f;
+
+	}
+
+	void Movement() {
+		transform.Translate(moveVector);
+	}
+}
