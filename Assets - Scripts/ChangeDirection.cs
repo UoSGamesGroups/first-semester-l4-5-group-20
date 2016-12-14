c8c87cbefc06877a8bb106c01fe1edadd7044780
E-Ward <s182964@ucs.ac.uk>
2016-12-14 10:10:39 +0000

    Makes the sprite face the way it is moving

---
+++ Assets - Scripts/ChangeDirection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeDirection : MonoBehaviour {
+
+	public Sprite BrainU,BrainR,BrainD,BrainL;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.W))
+			this.GetComponent<SpriteRenderer> ().sprite = BrainU;
+		if (Input.GetKey (KeyCode.S))
+			this.GetComponent<SpriteRenderer> ().sprite = BrainD;
+		if (Input.GetKey (KeyCode.D))
+			this.GetComponent<SpriteRenderer> ().sprite = BrainR;
+		if (Input.GetKey (KeyCode.A))
+			this.GetComponent<SpriteRenderer> ().sprite = BrainL;
+	}
+}
